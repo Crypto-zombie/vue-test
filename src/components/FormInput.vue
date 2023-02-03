8547b95ab64c1a811e936cac4b06d3f4353dc75b
Crypto-zombie <volcano103@gmail.com>
2023-02-03 16:43:42 +0200

    feat: add css for form

--- src/components/FormInput.vue
+++ src/components/FormInput.vue
@@ -13,18 +13,24 @@ const emit = defineEmits(['validate', 'update:modelValue'])
 </script>
 
 <template>
-  <div :class="[ 'form-group', !!error && 'has-error' ]">
-    <label class="form-label" for="email">{{label}}</label>
-    <input
-      :id="name"
-      :name="name"
-      :type="type"
-      :value="modelValue"
-      @input="$emit('update:modelValue', $event.target.value)"
-      class="form-input"
-      @blur="$emit('validate')"
-      @keypress="$emit('validate')"
-    />
+  <div>
+    <div class="form-group">
+      <label class="form-label" for="email">{{label}}</label>
+      <input
+        :id="name"
+        :name="name"
+        :type="type"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+        class="form-input"
+        @blur="$emit('validate')"
+        @keypress="$emit('validate')"
+      />
+    </div>
     <p class="form-input-hint" v-if="!!error">{{ error }}</p>
   </div>
 </template>
+<style scoped>
+
+
+</style>
